12d9fdc24c25df697baa22da1fbc2277c5286ad3
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2022-11-06 13:29:44 +0530

    code_fix_member1 done: code is good to go

--- prime.js
+++ prime.js
@@ -1,12 +1,11 @@
-function prime( number )
-{
+function prime(number) {
 
-let factors = 0;
-for ( let i=1;i<=number;i++)
-{
-    if(number%i==0){ factors++; }
-}
-factors==0?console.log("Yes Prime "): console.log("Not Prime");
+	let factors = 0;
+
+	for (let i = 1; i <= number; i++) {
+		if (number % i == 0) { factors++; }
+	}
+	factors > 2 ? console.log("Not Prime ") : console.log("Yes Prime");
 
 }
 
