4ca952446067b80d27a8549de941cbb91f536d14
Abhishek Patel <you@example.com>
2022-11-06 20:29:48 +0530

    Member 2 M2 Code Fix  Branch

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 function prime( number )
 {
 
-let factors = 0;
+let count = 0;
 for ( let i=1;i<=number;i++)
 {
-    if(number%i==0){ factors++; }
+    if(number%i==0){ count++; }
 }
-factors==0?console.log("Yes Prime "): console.log("Not Prime");
+count==2?console.log("Yes Prime "): console.log("Not Prime");
 
 }
 
